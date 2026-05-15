27222a943940cddad33b25edfa49c07adc442d3b
Johan <johan.gabrielson@gmail.com>
2026-05-15 10:58:19 +0200

    minor changes

--- modules/backend_asg/launch_template.tf
+++ modules/backend_asg/launch_template.tf
@@ -2,17 +2,16 @@ resource "aws_launch_template" "backend" {
   name_prefix   = "cloudcorp-backend-"
   image_id      = var.ami
   instance_type = var.instance_type
-
   iam_instance_profile {
     name = var.iam_instance_profile
   }
-
   network_interfaces {
     security_groups = [var.security_group_id]
   }
-
   user_data = base64encode(<<-EOF
 #!/bin/bash
+exec > /var/log/user-data.log 2>&1
+
 dnf update -y
 dnf install -y python3 python3-pip
 
@@ -21,8 +20,11 @@ cd /opt/backend
 
 cat << 'APP' > app.py
 from fastapi import FastAPI
-
 app = FastAPI()
+
+@app.get("/")
+def health():
+    return {"status": "ok"}
 
 @app.get("/api/message")
 def message():
@@ -31,13 +33,11 @@ APP
 
 pip3 install fastapi uvicorn --break-system-packages
 
-nohup uvicorn app:app --host 0.0.0.0 --port 8080 &
+nohup uvicorn app:app --host 0.0.0.0 --port 8080 > /var/log/uvicorn.log 2>&1 &
 EOF
   )
-
   tag_specifications {
     resource_type = "instance"
-
     tags = {
       Name        = "cloudcorp-backend"
       Environment = var.environment
